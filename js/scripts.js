772019ba7c082b2d2e2dca69286213469101aae0
Mollie Gower <mgower@eagles.ewu.edu>
2014-02-20 14:50:41 -0800

    version four

--- js/scripts.js
+++ js/scripts.js
@@ -4,6 +4,20 @@ $(document).foundation();
 // Your Awesome Scripts!
 $(document).ready(function(){
 
-	console.log('Page Loaded. Lets Do this!');
+// returns 0 to 'limit'
+var limit = 3;
+var numRand = Math.floor(Math.random()*limit);
+
+
+		var msg1 = 'HELLO';
+		var msg2 = 'HOLA';
+		var msg3 = 'BONJOUR';
+		var msg4 = 'NI HAO';
+
+
+
+	$('h1').text('msg' + numRand);
+
+	console.log(numRand);
 
 }); 
